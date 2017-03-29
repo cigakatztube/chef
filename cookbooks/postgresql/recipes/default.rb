779ae4e5d451e1926a356cf092648a1adadf451f
chef <chef@chef.com>
2017-03-29 10:00:12 +0000

    add postgresql

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,13 +5,13 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package 'postgresql-server' do
-notifies :run, 'execute [postgresql-init]'
+notifies :run, 'execute [postgresql-init]', :immediately
 end
 execute 'postgresql-init' do
 command 'postgresql-setup initdb'
 action :nothing
 end
 service 'postgresql' do
-action [:start, :enable]
+action [:enable, :start]
 end
 
